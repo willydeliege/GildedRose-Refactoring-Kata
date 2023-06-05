2db062bd1cd93911b0a7ab758c92aadaf1efb2b0
Frédéric Willem <frederic.willem@gmail.com>
2023-06-05 11:50:21 +0200

    add initial approval tests

---
+++ Java-Approvals/PROCESS.md
@@ -0,0 +1,4 @@
+# Step by step process to refactoring
+
+## Step 1
+Make sure the Approvals test are okay
--- Java-Approvals/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ Java-Approvals/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -1,38 +1,56 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.legacycode.Range;
+import org.approvaltests.reporters.DiffReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.junit.jupiter.api.Test;
+
+import java.awt.font.NumericShaper;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.approvaltests.Approvals;
-import org.approvaltests.reporters.DiffReporter;
-import org.approvaltests.reporters.UseReporter;
-import org.junit.jupiter.api.Test;
-import org.w3c.dom.Text;
-
 @UseReporter(DiffReporter.class)
 public class GildedRoseApprovalTest {
 
-	@Test
-	public void foo() {
+    @Test
+    public void foo() {
 
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-		Approvals.verifyAll("Items", items);
-	}
+        Approvals.verifyAll("Items", items);
+    }
 
     @Test
     public void thirtyDays() {
 
-        ByteArrayOutputStream fakeoutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(fakeoutput));
+        ByteArrayOutputStream fakeOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeOutput));
         System.setIn(new ByteArrayInputStream("a\n".getBytes()));
 
         Program.main();
-        String output = fakeoutput.toString();
+        String output = fakeOutput.toString();
 
         Approvals.verify(output);
     }
+
+    @Test
+    public void testAllCases() {
+        String[] names = {"Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+        Integer[] qualities = Range.get(1, 50);
+        Integer[] sellins = Range.get(-1, 50);
+        CombinationApprovals.verifyAllCombinations(this::updateItem, names, qualities, sellins);
+    }
+
+    private Item updateItem(String name, Integer quality, Integer sellins) {
+        Item[] items = new Item[]{new Item(name, sellins, quality)};
+        GildedRose gr = new GildedRose(items);
+        gr.updateQuality();
+        return items[0];
+    }
 }
+
