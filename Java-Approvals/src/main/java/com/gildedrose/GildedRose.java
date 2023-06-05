e0c69198a494f6aab2ed171c1c84496e0dee6b45
Frédéric Willem <frederic.willem@gmail.com>
2023-06-05 19:11:33 +0200

    isolate aged brie in first if stmnt

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -19,80 +19,38 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (item.name.equals(AGED_BRIE)) {
-                if (item.name.equals(AGED_BRIE)
-                    || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                            if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                    if (item.quality > 0) {
-                        if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        } else {
-                            item.quality = item.quality - 1;
-                        }
-                    }
                 }
 
-                if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                } else {
-                    item.sellIn = item.sellIn - 1;
-                }
+                item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
-                    if (!item.name.equals(AGED_BRIE)) {
-                        if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                            if (item.quality > 0) {
-                                if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
-                        } else {
-                            item.quality = 0;
-                        }
-                    } else {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             } else {
-                if (item.name.equals(AGED_BRIE)
-                    || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                            if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
-                    } else {
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                } else {
                     if (item.quality > 0) {
-                        if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        } else {
+                        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                             item.quality = item.quality - 1;
                         }
                     }
@@ -104,19 +62,14 @@ class GildedRose {
                 }
 
                 if (item.sellIn < 0) {
-                    if (!item.name.equals(AGED_BRIE)) {
-                        if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                            if (item.quality > 0) {
-                                if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
-                        } else {
-                            item.quality = 0;
-                        }
+                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        item.quality = 0;
                     } else {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                        if (item.quality > 0) {
+                            if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                                continue;
+                            }
+                            item.quality = item.quality - 1;
                         }
                     }
                 }
