02424b49b606c4a730470bc13b8d52a043470873
Frédéric Willem <frederic.willem@gmail.com>
2023-06-05 18:59:19 +0200

    prepare to isolate aged brie

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -18,60 +18,110 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            extracted(item);
-        }
-    }
+            if (item.name.equals(AGED_BRIE)) {
+                if (item.name.equals(AGED_BRIE)
+                    || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
 
-    private static void extracted(Item item) {
-        if (item.name.equals(AGED_BRIE)
-            || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                            if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                                if (item.sellIn < 11) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1;
+                                    }
+                                }
 
-                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                                if (item.sellIn < 6) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1;
+                                    }
+                                }
                             }
                         }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            } else {
-            if (item.quality > 0) {
-                if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                } else {
-                    item.quality = item.quality - 1;
-                }
-            }
-        }
-
-        if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-        } else {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                    } else {
                     if (item.quality > 0) {
-                        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                        if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                        } else {
                             item.quality = item.quality - 1;
                         }
                     }
+                }
+
+                if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                 } else {
-                    item.quality = 0;
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < 0) {
+                    if (!item.name.equals(AGED_BRIE)) {
+                        if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                            if (item.quality > 0) {
+                                if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                                    item.quality = item.quality - 1;
+                                }
+                            }
+                        } else {
+                            item.quality = 0;
+                        }
+                    } else {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                if (item.name.equals(AGED_BRIE)
+                    || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+
+                            if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                                if (item.sellIn < 11) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1;
+                                    }
+                                }
+
+                                if (item.sellIn < 6) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                    if (item.quality > 0) {
+                        if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                        } else {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                }
+
+                if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                } else {
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < 0) {
+                    if (!item.name.equals(AGED_BRIE)) {
+                        if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                            if (item.quality > 0) {
+                                if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                                    item.quality = item.quality - 1;
+                                }
+                            }
+                        } else {
+                            item.quality = 0;
+                        }
+                    } else {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
                 }
             }
         }
     }
+
 }
